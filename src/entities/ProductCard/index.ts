d6be1e4ada1f4dc59be0ce2b1c8535c3a3caf14e
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-15 22:39:41 +0600

    Create ProductCard

---
+++ src/entities/ProductCard/index.ts
@@ -0,0 +1,2 @@
+import ProductCard from "./ui/ProductCard";
+export {ProductCard}
---
+++ src/entities/ProductCard/ui/ProductCard.tsx
@@ -0,0 +1,12 @@
+interface IProductCard {
+
+
+}
+
+const ProductCard = ({}: IProductCard) => {
+  return (
+    <div>ProductCard</div>
+  )
+}
+
+export default ProductCard
