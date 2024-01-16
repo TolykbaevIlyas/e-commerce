4b55f84fae001b070a1a344d050ac08860dd778d
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-16 18:23:03 +0600

    Customize header

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,11 +1,34 @@
+import Image from "next/image"
+import Link from "next/link"
+
 interface IHeader {
 
 }
 
 const Header = ({}:IHeader) => {
   return (
-    <header>
-      Header
+    <header className="flex justify-between w-full items-center">
+      <div>
+        <Image alt="Logo" src=""/>
+      </div>
+      <nav className="flex justify-between">
+        <div className="">
+          <Link href="">Home</Link>
+        </div>
+        <div className="">
+          <Link href="">Products</Link>
+        </div>
+        <div className="">
+          <Link href="">Forum</Link>
+        </div>
+      </nav>
+      <div className="flex">
+        <Image alt="Cart" src=""/>
+        <Image alt="favorite" src=""/>
+      </div>
+      <div>
+        <Image alt="Profile" src=""/>
+      </div>
     </header>
   )
 }
