af266b79380e7bcda84e5772c724dab63772093e
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-23 23:30:07 +0600

    Create Products page

--- src/features/SearchInput/ui/SearchInput.tsx
+++ src/features/SearchInput/ui/SearchInput.tsx
@@ -2,8 +2,8 @@ import { Input } from "@/shared/ui/Input"
 
 const SearchInput = () => {
   return (
-    <div className="flex justify-center">
-      <Input className=" "/>
+    <div className="flex justify-center mt-5">
+      <Input className="text-center text-lg items-center m-auto w-96 p-2 rounded-lg text-black shadow-xl" placeholder="Enter product name"/>
     </div> 
   )
 }
