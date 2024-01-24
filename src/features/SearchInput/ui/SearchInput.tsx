0cf26a998c62c28e566c199d8f01cb00f32cdd80
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-24 23:34:59 +0600

    Update SearchInput.tsx

--- src/features/SearchInput/ui/SearchInput.tsx
+++ src/features/SearchInput/ui/SearchInput.tsx
@@ -1,9 +1,13 @@
 import { Input } from "@/shared/ui/Input"
+import { Button } from "@/shared/ui/button"
 
 const SearchInput = () => {
   return (
-    <div className="flex justify-center mt-5">
-      <Input className="text-center text-lg items-center m-auto w-96 p-2 rounded-lg text-black shadow-xl" placeholder="Enter product name"/>
+    <div >
+      <div className="flex justify-center pt-5">
+        <Input className="text-center text-lg items-center w-96 p-2 rounded-lg text-black shadow-xl" placeholder="Enter product name"/>
+        <Button className="items-center text-black shadow-xl ml-5">Search</Button>
+      </div>
     </div> 
   )
 }
